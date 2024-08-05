68d5fd936d08d12ff7a21dad6d7bf6f12985a921
Aluno SENAI <Aluno@SN-370698.edu.fiesc.com.br>
2024-08-05 08:57:57 -0300

    Estrutura Básica e Componentes

---
+++ src/Destino.jsx
@@ -0,0 +1,14 @@
+// Destino.js
+import React from 'react';
+
+const Destino = ({ nome, descricao }) => {
+  return (
+    <div>
+      <h2>{nome}</h2>
+      <p>{descricao}</p>
+    </div>
+  );
+};
+
+export default Destino;
+
